90ca6efcfb9b1459cd69d100dbb2e21b3c013975
Kuller <aku@sandag.org>
2016-02-29 16:42:26 -0800

    (ISAM-27) Initial commit test

---
+++ db/sql.py
@@ -0,0 +1,56 @@
+"""
+SQL queries to get input data for model
+variables: table names and rate versions(%s)
+values: declared in "model_config_yml"
+"""
+
+# BASE POPULATION (2010)
+population = """
+SELECT  age,
+        race_ethn,
+        sex,
+        type,
+        mildep,
+        persons,
+        households
+FROM    %s
+WHERE   bp_version = %s
+"""
+
+# MIGRATION RATES
+# for DIN(domestic in), DOUT(domestic out), FIN(foreign in), FOUT(foreign out)
+migration = """
+SELECT  rate_type AS migration,
+        yr,
+        age,
+        race_ethn,
+        sex,
+        rate
+FROM    %s
+WHERE   rate_type IN ('DIN','DOUT','FIN','FOUT') AND
+        rate_version = %s
+"""
+
+# BIRTH RATES
+birth = """
+SELECT  age,
+        race_ethn,
+        sex,
+        rate as birth_rate,
+        yr
+FROM    %s
+WHERE   rate_type = 'B' AND
+        rate_version = %s
+"""
+
+# DEATH RATES
+death = """
+SELECT  yr,
+        age,
+        race_ethn,
+        sex,
+        rate as death_rate
+FROM    %s
+WHERE   rate_type = 'D' AND
+        rate_version = %s
+"""
