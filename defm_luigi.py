2fed494710bff3463308602abb111af0d9998f4b
Marfatia <jma@sandag.org>
2017-05-26 15:02:49 -0700

    test luigi

--- defm_luigi.py
+++ defm_luigi.py
@@ -17,6 +17,9 @@ class Population(luigi.Task):
         return luigi.LocalTarget('temp/data.h5')
 
     def run(self):
+        # yr = pd.Series([2015])
+        # yr.to_hdf('temp/data.h5', 'year',  mode='w')
+        # print yr[0]
         pop = extract.create_df('population', 'population_table')
         pop.to_hdf('temp/data.h5', 'pop', format='table', mode='a')
 
@@ -40,15 +43,48 @@ class MigrationPopulationOut(luigi.Task):
                 'migration_rates': MigrationRates()
                 }
 
+    def output(self):
+        return luigi.LocalTarget('temp/data.h5')
+
     def run(self):
         mig_rates = pd.read_hdf('temp/data.h5', 'mig_rates')
         pop = pd.read_hdf('temp/data.h5', 'pop')
-        # pop = pop.set_index(['age', 'race_ethn', 'sex'])
-        # mig_rates = mig_rates.set_index(['age', 'race_ethn', 'sex'])
         yr_mig = compute.rates_for_yr(pop, mig_rates, 2015)
         mig_out = compute.net_mig(yr_mig, 1, 2015)
-        mig_out = mig_out[['mig_Dout','mig_Fout']]
+        mig_out = mig_out[['type', 'mildep', 'mig_Dout', 'mig_Fout']]
+        print mig_out.head()
+
         mig_out.to_hdf('temp/data.h5', 'mig_out', format='table', mode='a')
+
+
+class MigrationPopulationIn(luigi.Task):
+
+    def requires(self):
+        return {'population': Population(),
+                'migration_rates': MigrationRates()
+                }
+
+    def output(self):
+        return luigi.LocalTarget('temp/data.h5')
+
+    def run(self):
+        mig_rates = pd.read_hdf('temp/data.h5', 'mig_rates')
+        pop = pd.read_hdf('temp/data.h5', 'pop')
+        yr_mig = compute.rates_for_yr(pop, mig_rates, 2015)
+        mig_in = compute.net_mig(yr_mig, 1, 2015)
+        mig_in = mig_in[['type', 'mildep', 'mig_Din', 'mig_Fin']]
+        mig_in.to_hdf('temp/data.h5', 'mig_in', format='table', mode='a')
+
+
+class MigrationPopulationNet(luigi.Task):
+    def requires(self):
+        return {'mig_out': MigrationPopulationOut(),
+                'mig_in': MigrationPopulationIn()
+                }
+
+    def run(self):
+        mig_in = pd.read_hdf('temp/data.h5', 'mig_in')
+        mig_out = pd.read_hdf('temp/data.h5', 'mig_out')
 
 
 if __name__ == '__main__':
@@ -56,4 +92,4 @@ if __name__ == '__main__':
     shutil.rmtree('temp')
     os.makedirs('temp')
 
-    luigi.run(main_task_cls=MigrationPopulationOut)
+    luigi.run(main_task_cls=MigrationPopulationNet)
