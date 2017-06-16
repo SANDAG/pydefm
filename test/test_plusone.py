9278124c49f805d4a4367aa4ab2287e3aff73e2b
Anne Kuller <aku@sandag.org>
2017-06-16 16:28:47 -0700

    fix bug in test

--- test/test_plusone.py
+++ test/test_plusone.py
@@ -1,12 +1,12 @@
 import unittest
-from primes import is_prime
+from pydefm import plusone
 
 class PrimesTestCase(unittest.TestCase):
     """Tests for `primes.py`."""
 
     def test_is_five_prime(self):
         """Is five successfully determined to be prime?"""
-        self.assertTrue(is_prime(5))
+        self.assertTrue(plusone.is_prime(5))
 
 if __name__ == '__main__':
     unittest.main()
