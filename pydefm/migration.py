132e28aee9a7367dbbe9c5231ca74fa2bc926273
Anne Kuller <aku@sandag.org>
2017-06-21 12:30:17 -0700

    get rates for year of simulation

--- pydefm/migration.py
+++ pydefm/migration.py
@@ -13,3 +13,24 @@ def non_migrating_pop(non_mig_pop):
     non_mig_pop['mig_Fout'] = (non_mig_pop['persons'] * non_mig_pop['FOUT']).round()
     non_mig_pop['non_mig_pop'] = (non_mig_pop['persons'] - non_mig_pop['mig_Dout'] - non_mig_pop['mig_Fout']).round()
     return non_mig_pop
+
+
+def rates_for_yr(rates_all_years, sim_year):
+    """
+    Filter specific rates for a given year
+
+    Parameters
+    ----------
+    rates_all_years : pandas DataFrame
+        rates, to be filtered by year
+    sim_year : int
+        year being simulated
+
+    Returns
+    -------
+    pop_w_rates : pandas DataFrame
+        rates for a given year
+
+    """
+    rates_yr = rates_all_years[rates_all_years['yr'] == sim_year]
+    return rates_yr
