ead991529ce630f3e565c449b55fd6a45266a115
Anne Kuller <aku@sandag.org>
2017-06-21 09:06:44 -0700

    speed up travis and improve tests

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,26 +1,24 @@
 import pytest
-import numpy as np
 from pandas import DataFrame
 import pandas.util.testing as tm
 from pydefm import migration
 
 @pytest.fixture
 def pop():
-    return DataFrame({'a': [20, 10, 0]}, index=[2, 1, 0])
+    return DataFrame({'persons': [859, 73, 4493]}, index=[2, 1, 0])
 
 
 @pytest.fixture
 def m_rates():
-    return DataFrame({'b': [300, 100, 200]}, index=[3, 1, 2])
+    return DataFrame({'DOUT': [0.027663, 0.057815, 0.012424],'FOUT': [0.000000, 0.000000, 0.000000]}, index=[3, 1, 2])
 
 
 @pytest.mark.parametrize(
     "expected",
-    [(DataFrame({'a': [20, 10, 0],
-                                'b': [200, 100, np.nan]},
-                               index=[2, 1, 0]))])
-
+    [(DataFrame([[859,0.012424,0.000000],
+                 [73, 0.057815,0.000000],
+                 [4493, np.nan,np.nan]],
+                columns=['persons', 'DOUT','FOUT'], index=[2, 1, 0]))])
 def test_migration(pop, m_rates, expected):
-
     result =  migration.migrating_pop(pop, m_rates)
     tm.assert_frame_equal(result, expected)
