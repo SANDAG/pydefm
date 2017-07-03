41a38bf8eff043b9ac7493fa8beb6f106a5d8398
Anne Kuller <aku@sandag.org>
2017-07-03 16:48:27 -0700

    add test for non-migrating pop calculation

--- tests/test_migration_from_csv.py
+++ tests/test_migration_from_csv.py
@@ -2,6 +2,7 @@ import pytest
 import pandas as pd
 import pandas.util.testing as tm
 from pydefm import migration
+import numpy as np
 
 
 @pytest.fixture
@@ -11,15 +12,28 @@ def pop():
 
 @pytest.fixture
 def m_rates():
-    return pd.read_csv('tests/data/migration_rates.csv') # error here
+    return pd.read_csv('tests/data/migration_rates.csv')
+
+
+@pytest.fixture
+def joined_pop_rates():
+    return pd.read_csv('tests/data/population_w_migration_rates.csv')
 
 
 @pytest.mark.parametrize(
     "expected",
-    [pd.read_csv('tests/data/population_w_migration_rates.csv')]) # error is here in results
+    [pd.read_csv('tests/data/population_w_migration_rates.csv')])
 def test_migration(pop, m_rates, expected):
     pop = pop.set_index(['age', 'race_ethn', 'sex'])
     m_rates = m_rates.set_index(['age', 'race_ethn', 'sex'])
     expected = expected.set_index(['age', 'race_ethn', 'sex'])
     result =  migration.migrating_pop(pop, m_rates)
+    tm.assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "expected",
+    [pd.read_csv('tests/data/non_migrating_pop.csv',dtype = {'mig_Dout':  np.float64, 'non_mig_pop': np.float64})])
+def test_non_migrating_pop(joined_pop_rates, expected):
+    result = migration.non_migrating_pop(joined_pop_rates)
     tm.assert_frame_equal(result, expected)
