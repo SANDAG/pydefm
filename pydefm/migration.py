ca592224c8b78065bd4d28d03e286c332dc552b0
Anne Kuller <aku@sandag.org>
2017-07-06 09:48:36 -0700

    pep8 fixes

--- pydefm/migration.py
+++ pydefm/migration.py
@@ -9,12 +9,12 @@ def migrating_pop(population, migration_rates):
 
 def non_migrating_pop(non_mig_pop):
 
-    non_mig_pop['mig_Dout'] = (non_mig_pop['persons'] *\
+    non_mig_pop['mig_Dout'] = (non_mig_pop['persons'] *
                                non_mig_pop['DOUT']).round()
-    non_mig_pop['mig_Fout'] = (non_mig_pop['persons'] *\
+    non_mig_pop['mig_Fout'] = (non_mig_pop['persons'] *
                                non_mig_pop['FOUT']).round()
-    non_mig_pop['non_mig_pop'] = (non_mig_pop['persons'] -\
-                                  non_mig_pop['mig_Dout'] -\
+    non_mig_pop['non_mig_pop'] = (non_mig_pop['persons'] -
+                                  non_mig_pop['mig_Dout'] -
                                   non_mig_pop['mig_Fout']).round()
     return non_mig_pop
 
