cb68b7ea8312af1996b3177ea5031dfa97f66c7f
Anne Kuller <aku@sandag.org>
2017-07-13 13:29:24 -0700

    remove function that is no longer used

--- pydefm/utils.py
+++ pydefm/utils.py
@@ -26,25 +26,3 @@ def rates_for_yr(population, rates_all_years, sim_year):
     pop_w_rates = population.join(rates_yr)
     return pop_w_rates
 
-
-# join the rates with the population
-def population_w_rates(population, rates_for_yr):
-    """
-    Join rates for simulation year with population by cohort
-
-    Parameters
-    ----------
-    population : pandas DataFrame
-        population for simulated year, starting with base population
-
-    rates_for_yr : pandas DataFrame
-        rates for simulation year by cohort
-
-    Returns
-    -------
-    pop_w_rates : pandas DataFrame
-        rates for a given year and population for each cohort
-
-
-    """
-    return population.join(rates_for_yr, how='left', sort=False)
