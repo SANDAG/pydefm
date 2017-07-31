5a8deb0690a1315cf35b6722347cdff5992d0361
Marfatia <jma@sandag.org>
2017-07-31 15:25:46 -0700

    (ISAM-80) Interactive graphs to allow user to pick.

--- combined_simulation.py
+++ combined_simulation.py
@@ -8,16 +8,29 @@ import defm_luigi as defm
 import inc_luigi as inc
 import emp_luigi as emp
 from db import extract
-from db import sql
-from forecast import util
 import shutil
 import pandas
 import luigi.contrib.hadoop
 from sqlalchemy import create_engine
 from pysandag.database import get_connection_string
-from pysandag import database
+import pandas as pd
+from bokeh.models import (
+    ColumnDataSource, HoverTool, BoxZoomTool, WheelZoomTool, PanTool, SaveTool
+)
+from bokeh.charts import TimeSeries
+from bokeh.embed import components
+from db import sql
+from bokeh.resources import INLINE
+from bokeh.plotting import figure
+
 import warnings
 warnings.filterwarnings('ignore', category=pandas.io.pytables.PerformanceWarning)
+defm_engine = create_engine(get_connection_string("model_config.yml", 'output_database'))
+
+in_query = getattr(sql, 'max_run_id')
+db_run_id = pd.read_sql(in_query, defm_engine, index_col=None)
+
+run_id = db_run_id['id'].iloc[0]
 
 
 class CombinedSimulation(luigi.Task):
@@ -41,6 +54,23 @@ class CombinedSimulation(luigi.Task):
 app = Flask(__name__)
 
 
+def create_figure(df, current_feature_name):
+    p = figure(width=600, height=400, tools=[HoverTool(), BoxZoomTool(), WheelZoomTool(), PanTool(), SaveTool()],
+               title=current_feature_name, y_axis_label=current_feature_name,
+                 x_axis_label="Year")
+    p.line(df.index.tolist(), df[current_feature_name], line_width=2, legend=False, line_color="red")
+
+    source = ColumnDataSource(
+        {'x': df.index.values.tolist(), 'y': df[current_feature_name].values, 'y2': df[current_feature_name].map('{:,.0f}'.format).tolist()})
+
+    p.scatter('x', 'y', source=source, fill_alpha=0, line_alpha=.95, line_color="black", fill_color="blue")
+    hover = p.select(dict(type=HoverTool))
+    hover.tooltips = [("Year ", "@x"),  (current_feature_name, "@y2")]
+    hover.mode = 'mouse'
+    hover.line_policy = 'nearest'
+    return p
+
+
 @app.route('/')
 def my_form():
 
@@ -56,26 +86,104 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+        dem = request.form['dem']
+        econ = request.form['econ']
+        start_year = request.form['starty']
+        end_year = request.form['endy']
+        # os.system("luigid")
 
-    dem = request.form['dem']
-    econ = request.form['econ']
-    start_year = request.form['starty']
-    end_year = request.form['endy']
-    # os.system("luigid")
+        luigi.run(main_task_cls=CombinedSimulation, cmdline_args=['--start=' + str(start_year), '--end=' + str(end_year), '--dem=' + str(dem), '--econ=' + str(econ)])
 
-    luigi.run(main_task_cls=CombinedSimulation, cmdline_args=['--start=' + str(start_year), '--end=' + str(end_year), '--dem=' + str(dem), '--econ=' + str(econ)])
-    os.system("bokeh serve bokeh_graphs.py")
+        return redirect('/bokeh')
 
-    return '''
-    <form action="." method="BOKEH">
-         <div class="w3-row-padding">
-            <div class="w3-col l3 m6 w3-margin-bottom">
-                 <h5 class="w3-border-bottom w3-border-light-grey w3-padding-16">Click below to check results when the simulation is complete </h5>
-                 <input class="w3-button w3-black w3-section" type="submit" value="Bokeh Results" onclick="window.open('http://localhost:5006/bokeh_graphs')">
-            </div>
-         </div>
-    </form>" onclick="window.open('http://localhost:5006/bokeh_graphs')"> '''
 
+@app.route('/bokeh')
+def my_form_post2():
+        return render_template("bokeh-results.html")
+
+
+results_sql = '''SELECT "Population" as "Population"
+                        ,"Year"
+                        ,deaths_hhp_non_mil as "Deaths"
+                        ,mig_in - mig_out as "Net Migration"
+                        ,new_born as "Births"
+                FROM defm.population_summary
+                WHERE "Run_id" =''' + str(run_id)
+results_df = pd.read_sql(results_sql, defm_engine, index_col='Year')
+feature_names = results_df.columns[0:].values.tolist()
+
+
+@app.route('/bokeh/pop')
+def my_form_post_pop():
+
+    # Determine the selected feature
+    current_feature_name = request.args.get("feature_name")
+
+    if current_feature_name is None:
+        current_feature_name = "Population"
+
+    # Create the plot
+    plot1 = create_figure(results_df, current_feature_name)
+    # Embed plot into HTML via Flask Render
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(plot1)
+    html = render_template(
+        'result-form.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        feature_names=feature_names,
+        current_feature_name=current_feature_name
+    )
+    return html
+
+
+race_sql = '''SELECT  yr as "Year",
+        SUM(CASE WHEN race_ethn = 'B' THEN persons ELSE 0 END) as "Black",
+        SUM(CASE WHEN race_ethn = 'H' THEN persons ELSE 0 END) as "Hispanic",
+        SUM(CASE WHEN race_ethn = 'S' THEN persons ELSE 0 END) as "Asian",
+        SUM(CASE WHEN race_ethn = 'W' THEN persons ELSE 0 END) as "White",
+        SUM(CASE WHEN race_ethn = 'O' THEN persons ELSE 0 END) as "Other"
+        FROM defm.population
+        WHERE run_id=''' + str(run_id) + ''' GROUP BY yr  ORDER BY yr'''
+
+race_df = pd.read_sql(race_sql, defm_engine, index_col='Year')
+race_cat = race_df.columns[0:].values.tolist()
+
+
+@app.route('/bokeh/pop_age')
+def my_form_post_pop_by_race():
+
+    # Determine the selected feature
+    current_feature_name = request.args.get("feature_name")
+
+    if current_feature_name is None:
+        current_feature_name = "White"
+
+    # Create the plot
+    plot1 = create_figure(race_df, current_feature_name)
+    # Embed plot into HTML via Flask Render
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(plot1)
+    html = render_template(
+        'result-form-pop-race.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+        feature_names=race_cat,
+        current_feature_name=current_feature_name
+    )
+    return html
 
 if __name__ == '__main__':
     shutil.rmtree('temp')
